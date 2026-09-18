c8ad98dcd2e48fd6b24928d18d24d2734df7fea7
c-dez <c-dez@proton.me>
2026-09-18 02:31:35 -0600

    cambios menores

--- gameplay/addons/legs/legs_basic/resource_script/leg_stats_script.gd
+++ gameplay/addons/legs/legs_basic/resource_script/leg_stats_script.gd
@@ -2,16 +2,16 @@ extends Resource
 class_name LegStats
 
 
+@export var type := TYPE.basic
 @export var max_speed: float = 15
 @export var acceleration: float = 20.0
 @export var deceleration: float = 50.0
-
+@export_category('can_air_jump')
 @export var jump_mult: float = 1.5
 @export var max_air_jumps: int = 3
 @export var cool_down: float = 3.0
 
 
-@export var type := TYPE.basic
 enum TYPE {
     basic,
     aerial
