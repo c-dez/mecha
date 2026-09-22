b9d8cd6943fb82c42f10ac9940154485933273f6
c-dez <c-dez@proton.me>
2026-09-21 21:50:13 -0600

    renombrado nombres de nodos Timer

--- gameplay/mech_addons/back_packs/back_pack_basic/back_pack_basic.gd
+++ gameplay/mech_addons/back_packs/back_pack_basic/back_pack_basic.gd
@@ -13,10 +13,10 @@ class_name BackPack
 var input: Vector2
 var player: Player
 # instanciar nodos en script
-@onready var bost_timer: Timer = $BostTimer
+@onready var special_duration_timer: Timer = $SpecialDurationTimer
 @export var bost_time: float = 0.5
 
-@onready var recovery_timer: Timer = $RecoveryTimer
+@onready var special_recovery_timer: Timer = $SpecialRecoveryTimer
 @export var recovery_time: float = 0.1
 
 var last_direction := Vector3.ZERO
@@ -28,13 +28,10 @@ var signals_arr:Array = [
 
 
 func _ready() -> void:
-    if not owner is Player:
-        return
+    set_player()
+    special_duration_timer.connect('timeout', _on_bost_timeout)
 
-    player = owner
-    bost_timer.connect('timeout', _on_bost_timeout)
-
-    recovery_timer.connect('timeout', _on_recovery_timer_timeout)
+    special_recovery_timer.connect('timeout', _on_recovery_timer_timeout)
 
 
 func _physics_process(delta: float) -> void:
@@ -45,13 +42,13 @@ func _physics_process(delta: float) -> void:
     ).normalized()
 
     if Input.is_action_just_pressed(player.actions['back_pack']):
-        if bost_timer.is_stopped():
-            bost_timer.start(bost_time)
+        if special_duration_timer.is_stopped():
+            special_duration_timer.start(bost_time)
             is_bosting.emit(true)
             last_direction = direction
 
     # back bost(dash)
-    if bost_timer.time_left > 0.0 and input == Vector2.ZERO:
+    if special_duration_timer.time_left > 0.0 and input == Vector2.ZERO:
         var target_velocity := (player.transform.basis) * Vector3(
         0.0, 0.0, 1.0) * max_speed
 
@@ -66,7 +63,7 @@ func _physics_process(delta: float) -> void:
             acceleration * delta
         )
     # direction bost(dash)
-    if bost_timer.time_left > 0.0 and direction.length() > 0.0:
+    if special_duration_timer.time_left > 0.0 and direction.length() > 0.0:
         var target_velocity := last_direction * max_speed
 
         player.velocity.x = move_toward(
@@ -82,7 +79,7 @@ func _physics_process(delta: float) -> void:
 
         )
     # recovery
-    if recovery_timer.time_left > 0.0:
+    if special_recovery_timer.time_left > 0.0:
         player.velocity.x = move_toward(
             player.velocity.x,
             0.0,
@@ -96,10 +93,15 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_bost_timeout() -> void:
-    recovery_timer.start(recovery_time)
+    special_recovery_timer.start(recovery_time)
 
 
 func _on_recovery_timer_timeout() -> void:
     is_bosting.emit(false)
 
 
+func set_player()->void:
+    if not owner is Player:
+        return
+
+    player = owner
